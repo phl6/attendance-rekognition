642e44451bd05c365c0a7652480a711580fafded
phl6 <louisli1997@gmail.com>
2023-04-13 14:39:12 +0800

    tidy up lambdafunction

--- lamdaFunction/lamdafunction.py
+++ lamdaFunction/lamdafunction.py
@@ -35,30 +35,24 @@ def lambda_handler(event, context):
     print("Event:" + str(event))
     # Get the object from the event
     bucket = event['Records'][0]['s3']['bucket']['name']
-    print("bucket: " + bucket)
     key = event['Records'][0]['s3']['object']['key']
 
     try:
-        # Calls Amazon Rekognition IndexFaces API to detect faces in S3 object 
         # to index faces into specified collection
         response = index_faces(BUCKET_NAME, key)
-        print("Response1: " + str(response))
+        print("Response: " + str(response))
         
         # Commit faceId and full name object metadata to DynamoDB
         if response['ResponseMetadata']['HTTPStatusCode'] == 200:
             faceId = response['FaceRecords'][0]['Face']['FaceId']
-            print("faceId: " + str(faceId))
 
-            ret = s3.head_object(Bucket=bucket, Key=key)
-            print("ret: " + str(ret))
-            print("Metadata: " + str(ret['Metadata']))
-            personFullName = ret['Metadata']['fullname']
+            result = s3.head_object(Bucket=bucket, Key=key)
+            print("result: " + str(result))
+            personFullName = result['Metadata']['fullname']
 
+            # update dynamodb index
             dynamoUpdateResult = update_index(DYNAMO_TABLE_NAME, faceId, personFullName)
             print("dynamoUpdateResult" + str(dynamoUpdateResult))
-            
-        # Print response to console
-        print("Response2: " + str(response))
 
         return response
     except Exception as e:
